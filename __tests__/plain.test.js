659ed2d8728eec9a18c0dca8807555f17c3dc66e
Gregory Gapienko <skorpion1813@yandex.ru>
2023-10-01 23:59:18 +0300

    Add tests for plain formatter

---
+++ __tests__/plain.test.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@jest/globals';
+import genDiff from '../src/index.js';
+import { readFileSync } from 'node:fs';
+import path from 'path';
+import plain from '../src/formatters/plain.js';
+
+const getFixturePath = (filename) => path.resolve(`./__fixtures__/${filename}`);
+
+const jsonBefore = getFixturePath('before.json');
+const jsonAfter = getFixturePath('after.json');
+const yamlBefore = getFixturePath('before.yaml');
+const yamlAfter = getFixturePath('after.yaml');
+const result = readFileSync(getFixturePath('plainResult.txt'), 'utf-8');
+test('genDiff', () => {
+  expect(genDiff(jsonBefore, jsonAfter, plain)).toEqual(result);
+  expect(genDiff(yamlBefore, yamlAfter, plain)).toEqual(result);
+});
