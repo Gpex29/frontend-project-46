d2ec9a4c1ad24ce55ccade0ab7728c607cf1c11c
Gregory Gapienko <skorpion1813@yandex.ru>
2023-10-02 23:27:14 +0300

    Fix linter 3

--- src/getDiffTree.js
+++ src/getDiffTree.js
@@ -6,24 +6,25 @@ const getDiffTree = (before, after) => {
   const keys = Object.keys(obj);
   const sortedKeys = _.sortBy(keys);
   const result = sortedKeys.reduce((acc, key) => {
+    const newAcc = { ...acc };
     const oldFile = { ...before };
     const newFile = { ...after };
     const oldValue = oldFile[key];
     const newValue = newFile[key];
     if (isObject(oldValue) && isObject(newValue)) {
-      acc[key] = { status: 'merged', children: getDiffTree(oldValue, newValue) };
+      newAcc[key] = { status: 'merged', children: getDiffTree(oldValue, newValue) };
     } else if (!Object.hasOwn(oldFile, key)) {
-      acc[key] = { value: newValue, status: 'added' };
+      newAcc[key] = { value: newValue, status: 'added' };
     } else if (!Object.hasOwn(newFile, key)) {
-      acc[key] = { value: oldFile[key], status: 'deleted' };
+      newAcc[key] = { value: oldFile[key], status: 'deleted' };
     } else if (oldFile[key] !== newValue) {
-      acc[key] = {
+      newAcc[key] = {
         oldValue, newValue, status: 'changed',
       };
     } else {
-      acc[key] = { value: oldFile[key], status: 'unchanged' };
+      newAcc[key] = { value: oldFile[key], status: 'unchanged' };
     }
-    return acc;
+    return newAcc;
   }, {});
   return result;
 };
